f674236d907f78977d07581222a59822b75a77f1
MINSK\Alexey_Kachemirov <alexey_kachemirov@epam.com>
2018-04-20 12:34:36 +0300

    added pre-task_1 script

---
+++ Pre-task_1.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var arr = [];
+
+function createRandArr() {
+	for (var i = 1; i <= 10; i++) {
+		arr.push((Math.floor(Math.random() * 100))+1);
+	}
+}
+
+function compareNumbers(a, b) { 
+	return b - a;
+}
+
+createRandArr();
+arr.sort(compareNumbers);
+
+var mult = arr[0] * arr[1] * arr[2];
+
+alert( "Sorted array from max to min: \n" + arr );
+alert( "Multiply of 3 greatest values equals: \n" + mult );
